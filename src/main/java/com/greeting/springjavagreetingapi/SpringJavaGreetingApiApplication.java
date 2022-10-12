24bcf3d74015a85dfdb26748a7240320abc47980
TOVTC <veronica.tc.to@outlook.com>
2022-10-12 02:02:19 -0400

    initial commit

---
+++ src/main/java/com/greeting/springjavagreetingapi/SpringJavaGreetingApiApplication.java
@@ -0,0 +1,13 @@
+package com.greeting.springjavagreetingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaGreetingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaGreetingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greeting/springjavagreetingapi/SpringJavaGreetingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greeting.springjavagreetingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaGreetingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
